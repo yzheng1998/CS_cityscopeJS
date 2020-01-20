be430145d7984fab5dfa99ef59d694e4c7de57b4
Ariel Noyman <noyman@mit.edu>
2020-01-19 21:30:46 -0500

    Updates

---
+++ precache-manifest.4cf026b8f48a19550cc7da27db31bb98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8231fbf26f752ce01f55978cded1f87a",
+    "url": "/CS_cityscopeJS/index.html"
+  },
+  {
+    "revision": "121f07f6f28f53bb827a",
+    "url": "/CS_cityscopeJS/static/css/2.7a834bf8.chunk.css"
+  },
+  {
+    "revision": "0c36a45220e72f5323db",
+    "url": "/CS_cityscopeJS/static/css/main.bc180b82.chunk.css"
+  },
+  {
+    "revision": "121f07f6f28f53bb827a",
+    "url": "/CS_cityscopeJS/static/js/2.aa1fcdef.chunk.js"
+  },
+  {
+    "revision": "cbf4f58793ac3d0e69dbc5e76e6b36ed",
+    "url": "/CS_cityscopeJS/static/js/2.aa1fcdef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c36a45220e72f5323db",
+    "url": "/CS_cityscopeJS/static/js/main.ab696e4c.chunk.js"
+  },
+  {
+    "revision": "97b8454917c422f81a9c",
+    "url": "/CS_cityscopeJS/static/js/runtime-main.98995e76.js"
+  }
+]);
