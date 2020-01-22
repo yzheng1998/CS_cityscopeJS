b590b8b5680f995dc9d9b7305c3ab0a312d17be7
Ariel Noyman <noyman@mit.edu>
2020-01-22 14:07:47 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CS_cityscopeJS/precache-manifest.5537ab6698f4d29f70025db90875a79f.js"
+  "/CS_cityscopeJS/precache-manifest.792e9aa19916f86e7237718dc49ed873.js"
 );
 
 self.addEventListener('message', (event) => {
